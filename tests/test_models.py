faeb65ee5a309699d1ba25c607622773b6186a55
Michael Bratsch <michael.bratsch83@gmail.com>
2016-02-28 21:47:03 +0100

    add tests for some views

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,37 +3,41 @@ from register.models import Candidate, CandidateMetaData
 
 from itertools import product
 
-my_test_candidates = [
-    {'first_name' : 'Stefan', 'last_name' : 'Mueller',
-     'email' : 'stefan@mueller.net'},
-    {'first_name' : 'Florian', 'last_name' : 'Klaus',
-     'email' : 'florian.klaus@gmail.com'}]
+class CandidateTestBase(TestCase):
+    def __init__(self, *args, **kwargs):
+        TestCase.__init__(self, *args, **kwargs)
+        self.test_candidates = [{'first_name' : 'Stefan', 'last_name' : 'Mueller',
+                                 'email' : 'stefan@mueller.net'},
+                                {'first_name' : 'Florian', 'last_name' : 'Klaus',
+                                 'email' : 'florian.klaus@gmail.com'}]
+        n_times = 20
+        self.test_candidates = n_times*self.test_candidates
 
-n_times = 50
 
-class CandidateTestCase(TestCase):
+class CandidateTestCase(CandidateTestBase):
     def setUp(self):
-        for candidate, _ in product(my_test_candidates, range(n_times)):
+        for candidate in self.test_candidates:
             Candidate.objects.create(**candidate)
 
     def test_number_of_candidates(self):
-        self.assertEqual(len(Candidate.objects.all()),
-                         len(my_test_candidates)*n_times)
+        self.assertEqual(len(Candidate.objects.all()), len(self.test_candidates))
 
-class CandidateMetaDataTestCase(TestCase):
+
+class CandidateMetaDataTestBase(CandidateTestBase):
     def setUp(self):
-        for candidate, _ in product(my_test_candidates, range(n_times)):
+        for candidate in self.test_candidates:
             candidate_object = Candidate.objects.create(**candidate)
             candidate_meta_data_object = CandidateMetaData.create(candidate_object)
             candidate_meta_data_object.save()
 
+
+class CandidateMetaDataTestCase(CandidateMetaDataTestBase):
     def test_number_of_meta_candidates(self):
         self.assertEqual(len(CandidateMetaData.objects.all()),
-                         len(my_test_candidates)*n_times)
+                         len(self.test_candidates))
 
     def test_unique_identifiers(self):
         candidate_meta_data_list = list(CandidateMetaData.objects.all())
         identifier_set = set(i.identifier for i in candidate_meta_data_list)
-        self.assertEqual(len(identifier_set),
-                         len(my_test_candidates)*n_times)
+        self.assertEqual(len(identifier_set), len(self.test_candidates))
 
---
+++ tests/test_views.py
@@ -0,0 +1,36 @@
+from django.test import Client, TestCase
+from register.models import CandidateMetaData
+from tests.test_models import CandidateMetaDataTestBase
+
+import re
+
+class ViewTestCase(CandidateMetaDataTestBase):
+    def get_match(self, url, params, pattern):
+        client = Client()
+        response = client.get(url, params)
+        self.assertEquals(response.status_code, 200)
+
+        return re.search(pattern, response.content)
+
+
+class ThanksViewTestCase(ViewTestCase):
+    def test_total_number_in_line(self):
+        match = self.getMatch(url     = '/register/thanks.html',
+                              params  = None,
+                              pattern = 'total number of (\d+) people')
+        self.assertEquals(len(self.test_candidates), int(match.group(1)))
+
+
+class NumberInLine(ViewTestCase):
+    def test_wrong_user_id(self):
+        client = Client()
+        response = client.get('/register/current-in-line.html')
+        self.assertEquals(response.status_code, 400)
+
+    def test_all_user_ids(self):
+        for num, candidate in enumerate(CandidateMetaData.objects.all()):
+            match = self.getMatch(url     = '/register/current-in-line.html',
+                                  params  = {'user_id' : candidate.identifier},
+                                  pattern = 'Currently you are number (\d+)')
+            self.assertEquals(num+1, int(match.group(1)))
+
