cd1f003d11b08b222abdb64166d853922d31e057
Hannes Riehl <hannes.riehl@posteo.de>
2016-05-13 15:56:54 +0200

    add email settings

--- bwb/email_settings_template.py
+++ bwb/email_settings_template.py
@@ -1,6 +1,5 @@
-
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'abc@gmail.com'
-EMAIL_HOST_PASSWORD = 'asdfqwer'
+EMAIL_HOST = 'mail.tiggerswelt.net'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = 'username'
+EMAIL_HOST_PASSWORD = 'password'
