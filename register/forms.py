f6b6280bca38f648229b7b26550a0a133f229bd0
Hannes Riehl <hannes.riehl@posteo.de>
2016-05-25 17:17:32 +0200

    make either email address or mobile number required

--- register/forms.py
+++ register/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.translation import ugettext
 
 from register.models import max_name_length, User_Registration
 
@@ -8,7 +9,19 @@ class RegistrationForm(forms.Form):
     last_name = forms.CharField(max_length=max_name_length, required=True)
     date_of_birth = forms.DateField(required=True)
 
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(required=False)
+    mobile_number = forms.CharField(max_length=15, required=False)
+
     bicycle_kind = forms.ChoiceField(
         choices=User_Registration.BICYCLE_CHOICES,
         required=True)
+
+    def clean(self):
+        email = self.cleaned_data.get('email')
+        mobile_number = self.cleaned_data.get('mobile_number')
+
+        if not (email or subject):
+            raise forms.ValidationError(ugettext(
+                "Either a email address or a mobile number is required for contacting you!"))
+
+        return self.cleaned_data
