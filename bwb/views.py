1a708209d16e3597e43e4d949518c0534779fb62
Michael Bratsch <michael.bratsch83@gmail.com>
2016-06-02 21:52:36 +0200

    redirect to current page after switching languages

--- bwb/views.py
+++ bwb/views.py
@@ -1,10 +1,5 @@
-from django.http import HttpResponseBadRequest
-from django.shortcuts import render, redirect
-from django.utils import translation
-
+from django.shortcuts import render
 from django.views.generic import View
-
-from bwb.settings import LANGUAGES
 
 
 class GreetingsView(View):
@@ -13,14 +8,3 @@ class GreetingsView(View):
     def get(self, request, *args, **kwargs):
         context_dict = {'show_steps': True}
         return render(request, self.template_name, context_dict)
-
-    def post(self, request, *args, **kwargs):
-        user_language = request.POST.get('language')
-
-        if user_language not in [language for language, _ in LANGUAGES]:
-            return HttpResponseBadRequest()
-
-        translation.activate(user_language)
-        request.session[translation.LANGUAGE_SESSION_KEY] = user_language
-
-        return redirect('register:greeting')
