f589131ae04e368e1e4984390334742cddd78ebd
Hannes Riehl <hannes.riehl@posteo.de>
2016-06-27 20:48:23 +0200

    cosmetic changes

--- bwb/settings.py
+++ bwb/settings.py
@@ -160,4 +160,4 @@ CRISPY_TEMPLATE_PACK = 'bootstrap3'
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'), )
